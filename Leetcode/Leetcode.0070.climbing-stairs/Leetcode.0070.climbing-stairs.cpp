5035f6e0399c542788d243e76bd925d06b16feb8
Zugz Wang <aldrich0wang@gmail.com>
2022-07-31 16:54:00 -0500

    Added Leetcode, problem 70, climbing stairs

---
+++ Leetcode/Leetcode.0070.climbing-stairs/Leetcode.0070.climbing-stairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int array[46];
+        array[0] = 1; array[1] = 1; // Base cases
+        for(int i = 2; i <= n; i++) {
+            array[i] = array[i - 1] + array[i - 2]; // Recursive definition
+        }
+        return array[n];
+    }
+};
