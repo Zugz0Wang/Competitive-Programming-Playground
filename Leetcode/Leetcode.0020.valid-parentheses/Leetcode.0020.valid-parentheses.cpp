f9a0f2652d34e5a2b484b4095dcc518291b2b7c4
Zugz Wang <aldrich0wang@gmail.com>
2022-08-06 17:31:30 -0500

    Added Leetcode.0020

---
+++ Leetcode/Leetcode.0020.valid-parentheses/Leetcode.0020.valid-parentheses.cpp
@@ -0,0 +1,35 @@
+#include <stack>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        const int kLength = s.length();
+        for(int i = 0; i < kLength; i++) {
+            char curr_char = s[i];
+            if (curr_char == '(' || curr_char == '[' || curr_char == '{') {
+                stk.push(curr_char);
+            } else {
+                if (stk.size() == 0) {
+                    return false; // if the stack is empty, not match
+                }
+                char prev_char = stk.top();
+                if (prev_char == '(' && curr_char != ')') { // if the previous bracket doesn't match
+                    return false;
+                } else if (prev_char == '[' && curr_char != ']') {
+                    return false;
+                } else if (prev_char == '{' && curr_char != '}') {
+                    return false;
+                }
+                stk.pop();
+            }
+        }
+        if (stk.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+};
