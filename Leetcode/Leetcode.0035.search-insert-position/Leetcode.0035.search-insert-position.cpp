5ffbde88e4d449c70fa5fb3d2365f57a96d94661
Zugz Wang <aldrich0wang@gmail.com>
2022-08-07 10:24:44 -0500

    search-insert-position

---
+++ Leetcode/Leetcode.0035.search-insert-position/Leetcode.0035.search-insert-position.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int head = 0;
+        int mid = 0;
+        int tail = nums.size();
+        if (target < nums.at(head)) {
+            return 0;
+        }
+        if (target > nums.at(tail - 1)) {
+            return tail;
+        }
+        while (head < tail) { // Binary search
+            mid = (head + tail) / 2;
+            // cout << head << ' ' << mid << ' ' << tail << '\n';
+            int curr_int = nums.at(mid);
+            if (curr_int == target) {
+                return mid;
+            } else if (curr_int < target) {
+                head = mid + 1;
+            } else {
+                tail = mid;
+            }
+        }
+        mid = (head + tail) / 2; // Update mid
+        return mid;
+    }
+};
